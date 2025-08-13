990759308d52ce66f58903cc11944f4e997d0e7f
utkarsh raj <utkarshraj525@gmail.com>
2025-08-13 10:07:01 +0530

    Craeted Category Section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { Navbar } from './Components/Navbar'
+import CategorySection from './Pages/CategorySection'
 import { FeaturedSection } from './Pages/FeaturedSection'
 import { HomePage } from './Pages/HomePage'
 
@@ -9,6 +10,7 @@ function App () {
       <Navbar />
       <HomePage />
       <FeaturedSection />
+      <CategorySection />
     </>
   )
 }
--- frontend/src/Pages/FeaturedSection.jsx
+++ frontend/src/Pages/FeaturedSection.jsx
@@ -5,7 +5,7 @@ import { CardBooks } from '../Components/CardBooks'
 
 export const FeaturedSection = () => {
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+    <div className='max-w-7xl mx-auto px-6 sm:px-6 lg:px-8'>
       <div className='flex justify-between items-center p-6 rounded-lg'>
         <div className='text-[var(--foreground)]'>
           <h2 className='text-2xl font-bold mb-2'>Featured Books</h2>
